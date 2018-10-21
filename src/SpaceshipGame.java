ab33774251136e64cb3ee4205d939375bec37cc3
Talos10 <40527607+Talos10@users.noreply.github.com>
2018-10-20 20:21:18 -0400

    Spaceship moves boii

--- src/SpaceshipGame.java
+++ src/SpaceshipGame.java
@@ -13,6 +13,8 @@ import org.newdawn.slick.tiled.TiledMap;
 public class SpaceshipGame extends BasicGame
 {
     private Image SPACEMAP;
+    private Image SPACESHIP;
+    private float x, y;
 
     public SpaceshipGame()
     {
@@ -26,6 +28,7 @@ public class SpaceshipGame extends BasicGame
             AppGameContainer app = new AppGameContainer(new SpaceshipGame());
             app.setDisplayMode(1000, 800, false);
             app.start();
+            app.setTargetFrameRate(60);
         }
         catch (SlickException e)
         {
@@ -36,14 +39,22 @@ public class SpaceshipGame extends BasicGame
     @Override
     public void init(GameContainer container) throws SlickException {
         SPACEMAP = new Image("SPACEMAP.jpg");
+        SPACESHIP = new Image("SPACESHIP.gif");
+        x=0;
+        y=0;
     }
 
     @Override
     public void update(GameContainer container, int delta) throws SlickException {
+        x += 0.1 * delta;
+        y += 0.1 * delta;
+        //SPACESHIP.rotate(delta);
     }
 
     public void render(GameContainer container, Graphics g) throws SlickException {
         SPACEMAP.draw();
+        SPACESHIP.draw(x,y);
+
     }
 }
 
